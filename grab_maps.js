bf5e8c224c33be58b4d2cae86c1282b9d61ada30
Bryan Wilson <bryanlandia@gmail.com>
2016-08-08 18:28:43 -0700

    stubs for some overlays, lots of city coordinates

--- grab_maps.js
+++ grab_maps.js
@@ -2,36 +2,49 @@ const tile_width = 100;
 const tile_height = 100;
 const tile_scale = 1; 
 const gmaps_map_zoom = 14;
-const gmaps_map_types = new Array("terrain", "terrain", "terrain", "terrain", "terrain"); //("streetmap", "satellite", "hybrid", "terrain");
+const gmaps_map_types = new Array("terrain"); //("streetmap", "satellite", "hybrid", "terrain");
 const gmaps_vertical_crop = 22;
 const gmaps_road_weight = 4;
 const gmaps_road_color = "0x000000";
 const gmaps_road_style = {'weight': gmaps_road_weight, 'color': gmaps_road_color};
-// const gmaps_water_stroke_weight = 1;
-// const gmaps_water_stroke_color = "0xFFFFFF";
-// const gmaps_water_style = {'weight': gmaps_water_stroke_weight, 'color': gmaps_water_stroke_color};
+const gmaps_water_stroke_weight = 1;
+const gmaps_water_stroke_color = "0xEEEEEE";
+const gmaps_water_style = {'weight': gmaps_water_stroke_weight, 'color': gmaps_water_stroke_color};
 // const gmaps_land_
-const gmaps_base_url = "https://maps.googleapis.com/maps/api/";
+const gmaps_api_type = "staticmap";            
+const gmaps_base_url = "https://maps.googleapis.com/maps/api/"+gmaps_api_type+"?key="+gmaps_static_api_key;
 const gmaps_api_types = {"streetview": {"latlng_param_name": "location"},
                          "staticmap": {"latlng_param_name": "center"}
                         };
-const gmaps_api_type = "staticmap";               
+   
 
 // 47.7165502,-122.3240542 (north Seattle)
-const start_max_lat = 47.72;
-const start_min_lat = 47.71;
-const start_max_lng = -122.38;
-const start_min_lng = -122.27;
-const offsetDivisor = 5000;
+// 37.783333, -122.416667	(San Fran.)
+// 34.05, -118.25 (L.A.)
+// 41.836944, -87.684722 (Chicago)
+// 32.715, -117.1625 (San Diego)
+// 39.76185, -104.881105 (Denver)
+// 40.7127, -74.0059 (NYC)
+// 28.415833, -81.298889 (Orlando)
+// 45.5331105,-73.6121502 (Montreal)
+// 64.8385258,-147.6863164 (Fairbanks)
 
+const start_max_lat = 64.9;// mntqc45.6;// orlando28.6;// nyc40.951;// denver39.8;// sandiego32.55;// chi41.85;// la34.3; //sf37.81;//sea47.72;
+const start_min_lat = 64.78;// mntqc45.5;// orlando28.5;// nyc40.71;// denver39.7;// sandiego32.70;// chi41.825;// la34.2; //sf37.79; //sea47.71;
+const start_max_lng = -147.6;// mntqc-73.4;// orlando-81.2;// nyc-73.8;// denver-104.82;// sandiego-117.0;// chi-87.60;// la-118.1; //sf-122.4; //sea-122.32;
+const start_min_lng = -147.9;// mntqc-73.7;// orlando-81.45;// nyc-74.1;// denver-105.2;// sandiego-117.2;// chi-87.72;// la-118.8; //sf-122.42; //sea-122.31;
+const offsetDivisor = 5000;
 
 
 var gmaps_api_obj = eval("gmaps_api_types."+gmaps_api_type);
 var styles = (Boolean(gmaps_road_style)) ? "style=feature:road|element:geometry|color:"+gmaps_road_style.color+"|weight:"+gmaps_road_style.weight : "";
-// styles += (Boolean(gmaps_water_style)) ? "&style=feature:landscape.naturl|element:geometry.stroke|color:"+gmaps_water_style.color+"|weight:"+gmaps_water_style.weight: "";
+// styles += (Boolean(gmaps_water_style)) ? "&style=feature:water|element:geometry.fill|color:"+gmaps_water_style.color+"|weight:"+gmaps_water_style.weight: "";
+styles += "&style=feature:poi.park|element:all|color:0xFF0000";
+styles += "&style=feature:landscape.manmade|element:all|color:0xFF0000&";
 styles += "&";
-var gmaps_url = gmaps_base_url + gmaps_api_type + "?"+ styles;
+var gmaps_url = gmaps_base_url + "&"+ styles;
 var latlng_param_name = gmaps_api_obj.latlng_param_name;
+var destination_latlng = {'lat':start_max_lat-25,'lng':start_max_lng};
 
 (function($) {
 
@@ -43,17 +56,74 @@ var latlng_param_name = gmaps_api_obj.latlng_param_name;
 	var tile_div = $('#map-tiles');
 	var horiz = Math.ceil($('body').width() / tile_width);
 	var vert = Math.ceil($('body').height() / (tile_height-gmaps_vertical_crop));
-	var lastScrollTop, label_num=0, latlng_last_marker, latlng_next_marker;
+	var lastScrollTop, label_num=0, latlng_last_marker, latlng_next_marker, all_lat_lngs=new Array;
 
-	$(document).ready(function(){ load_tiles()});
+	$(document).ready(function(){ 
+		// load_overlay_outline(); 
+		load_tiles();
+	});
+
+	var LA_offsetX = 0;
+
 	$(window).scroll(function() {
 		if ($(window).scrollTop() <= lastScrollTop) return; // only load new on scroll down
-		load_tiles($(window).scrollTop(), $(window).scrollLeft())
+		offsetX = $(window).scrollLeft();
+		// for LA so we don't immediately hit the ocean
+		//offsetX -= 20;
+		load_tiles($(window).scrollTop(), offsetX);
+		//load_overlay_paths($(window).scrollTop(), $(window).scrollLeft());
 	});
+
+	function load_overlay_outline() {
+		// load a shape outline transparent png
+		var body_w = window.outerWidth;
+		var body_h = window.outerHeight;
+		var url = gmaps_base_url + "&format=png32&size="+Math.ceil(body_w/2)+"x"+Math.ceil(body_h/2);
+		url += "&scale=2&center=Seattle,WA&visible=Seattle,WA";
+		url += "&style=feature:all|element:labels|visibility:off";
+		url += "&style=feature:water|element:all|visibility:on";
+		url += "&style=feature:road|element:all|visibility:off";
+		url += "&style=feature:poi|element:all|visibility:off";
+		url += "&style=feature:transit|element:all|visibility:off";
+		url += "&style=feature:landscape|element:geometry.fill|color:0xFFFFFF";
+		// url += "&style=feature:landscape|element:geometry.stroke|visibility:on|width:4|color:0x000000";
+		// url += "&style=feature:administrative|element:geometry.stroke|color:0x000000|weight:8|visibility:on";
+		var img_str = "<img src='"+url+"'/>";
+		img = $(img_str);
+		img.css("position","fixed")
+		   .css("z-index","1")
+		   .css("left",0)
+		   .css("top",0)
+		   .css("width",body_w)
+		   .css("height",body_h);
+		$("body").append(img);
+	}
+
+	function load_overlay(offsetY=0, offsetX=0) {
+		console.log('loading overlay image.  offsetY:'+offsetY+', offsetX:'+offsetX);
+		var body_w = window.outerWidth;
+		var body_h = window.outerHeight;
+		var path_str = "&path=color:0xFF0000|weight:2";
+		var styles = "&style=feature:all|visibility:off";
+		for (i=0;i<all_lat_lngs.length;i++) {
+			latlng = all_lat_lngs.shift();
+			path_str += "|"+latlng.lat+","+latlng.lng;
+		}
+		var url = gmaps_url + "format=png32&size="+body_w+"x"+body_h + path_str + styles;
+		var img_str = "<img src='"+url+"'/>";
+		img = $(img_str);
+		img.css("position","absolute")
+		   .css("z-index","1")
+		   .css("left",offsetX)
+		   .css("top",offsetY)
+		   .css("width",body_w)
+		   .css("height",body_h);
+		$("body").append(img);
+	}
 
 	function load_tiles(offsetY=0, offsetX=0) {
 		// debugger;
-		console.log('loading new tiles.  offsetY:'+offsetY+', offsetX:'+offsetX);
+		//console.log('loading new tiles.  offsetY:'+offsetY+', offsetX:'+offsetX);
 		var row, markers, latlng, img_str, img, img_container, paths, path_weight, has_marker=false;
 
 		for (var v=1; v<=vert; v++) {
@@ -65,7 +135,7 @@ var latlng_param_name = gmaps_api_obj.latlng_param_name;
 				maptype = Math.floor(random()*gmaps_map_types.length);
 				maptype = gmaps_map_types[maptype];
 				//console.log(maptype);
-				geo = {max_lat: max_lat-(offsetY/offsetDivisor), min_lat: min_lat-(offsetY/offsetDivisor), max_lng: max_lng+offsetX, min_lng: min_lng+offsetX};
+				geo = {max_lat: max_lat-(offsetY/offsetDivisor), min_lat: min_lat-(offsetY/offsetDivisor), max_lng: max_lng+(offsetX/offsetDivisor), min_lng: min_lng+(offsetX/offsetDivisor)};
 				latlng = randLatLng(geo);
 				if (random() <= 0.02) {
 					has_marker = true;
@@ -75,8 +145,8 @@ var latlng_param_name = gmaps_api_obj.latlng_param_name;
 					label_num++;
 				}
 				else markers = "";
-				paths = (latlng_last_marker && has_marker) ? "path=color:0xFF0000|weight:"+path_weight+"|"+latlng.lat+","+latlng.lng+"|"+latlng_last_marker.lat+","+latlng_last_marker.lng+"&" : "";
-				url = gmaps_url + markers + paths + latlng_param_name + "="+latlng.lat+","+latlng.lng+"&size="+tile_width+"x"+tile_height+"&maptype="+maptype+"&zoom="+gmaps_map_zoom+"&key="+gmaps_static_api_key;
+				paths = (latlng_last_marker && has_marker) ? "path=color:0xFF0000|weight:"+path_weight+"|"+destination_latlng.lat+","+destination_latlng.lng+"|"+latlng.lat+","+latlng.lng+"|"+latlng_last_marker.lat+","+latlng_last_marker.lng+"&" : "";
+				url = gmaps_url + markers + paths + latlng_param_name + "="+latlng.lat+","+latlng.lng+"&size="+tile_width+"x"+tile_height+"&maptype="+maptype+"&zoom="+gmaps_map_zoom;
 				img_str = "<img src='"+url+"'/>";
 				img = $(img_str);
 				img_container = $("<div class='img-container'/>");
@@ -84,7 +154,8 @@ var latlng_param_name = gmaps_api_obj.latlng_param_name;
 				if (h % 2 == 0) img_container.addClass('odd');
 				img_container.append(img);
 				row.append(img_container);	
-				if (has_marker) latlng_last_marker = latlng;	
+				if (has_marker) latlng_last_marker = latlng;
+				all_lat_lngs.push(latlng);	
 			}
 		}
 		tile_div.css('height', tile_div.height()*2);
